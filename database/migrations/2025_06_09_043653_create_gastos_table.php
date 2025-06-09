9d8dff12b82c4e7c2d6a99755230dc1403841004
Meybell25 <meybell01quintanilla@gmail.com>
2025-06-08 22:58:18 -0600

    Primer commit del proyecto Laravel Carwash

---
+++ database/migrations/2025_06_09_043653_create_gastos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('gastos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('set null');
+
+            $table->enum('tipo', ['stock','sueldos','personal','mantenimiento','otro']);
+            $table->string('detalle');
+            $table->float('monto');
+            $table->timestamp('fecha_gasto')->useCurrent();
+
+            $table->timestamps(); // created_at, updated_at
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gastos');
+    }
+};
