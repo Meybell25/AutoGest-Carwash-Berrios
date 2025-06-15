d8e4ff7519705d4737197b0f3ad2b40176c7a0da
Meybell25 <meybell01quintanilla@gmail.com>
2025-06-15 17:52:51 -0600

    Actualiza configuración para producción

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,18 +3,29 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class Authenticate
+class Authenticate extends Middleware
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    protected function redirectTo(Request $request): ?string
     {
+        if (!$request->expectsJson()) {
+            return route('login');
+        }
+        
+        return null;
+    }
+    
+    public function handle($request, Closure $next, ...$guards): Response
+    {
+        $this->authenticate($request, $guards);
+
         return $next($request);
     }
 }
