59bb7b8290174a9d6f960f522e601121e6551ec8
Yahir Nieto <94183916+YANA021@users.noreply.github.com>
2025-06-18 13:04:31 -0600

    prueba

--- resources/views/cliente/dashboard.blade.php
+++ resources/views/cliente/dashboard.blade.php
@@ -8,6 +8,7 @@
     <title>Dashboard Cliente - Carwash Berríos</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    //
     <style>
         * {
             margin: 0;
