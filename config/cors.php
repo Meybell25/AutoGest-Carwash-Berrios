e20ab2cba00b6eb68b8493385e42d9e77cafd28f
Meybell25 <meybell01quintanilla@gmail.com>
2025-06-21 01:07:00 -0600

    CORRIGIENDO

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,12 @@
+<?php
+
 return [
     'paths' => ['api/*', 'broadcasting/*', 'sanctum/*'],
-    'allowed_origins' => ['https://autogest-carwash-berrios-production.up.railway.app'],
     'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://autogest-carwash-berrios-production.up.railway.app'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
